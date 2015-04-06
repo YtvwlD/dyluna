4725e294f850cd34b8efe03d6b04ed27bddc0be1
Niklas Sombert <niklas@ytvwld.de>
2015-04-06 10:35:02 +0200

    Compute the string needed for the generation of the sids.

--- ajax.py
+++ ajax.py
@@ -24,8 +24,14 @@ def run(request):
 		if do[1] == "create":
 			#taken from http://love-python.blogspot.de/2010/04/python-code-to-generate-random-string.html
 			sid = ''
+			randstr = "abcdefghijklmnopqrstuvwxyz"
+			randstr += randstr.upper()
+			randints = ""
+			for n in range(10):
+				randints += str(n)
+			randstr += randints * 17
 			for i in range(32):
-				sid += random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789')
+				sid += random.choice(randstr)
 			#end taken
 			cur.execute ("INSERT INTO sessions (sid, uid, oid) VALUES (%s, NULL, '')", (sid,))
 
