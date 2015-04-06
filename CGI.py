d58312343eec1486936b1805f378148362e3f74d
Niklas Sombert <niklas@ytvwld.de>
2015-04-06 10:15:34 +0200

    i10n: the 500 error page.

--- CGI.py
+++ CGI.py
@@ -2,7 +2,7 @@
 from wsgiref.handlers import CGIHandler
 from werkzeug.wrappers import Response
 
-from get_html import get_html
+from get_html import get_html, choose_lang
 
 app = None
 
@@ -16,12 +16,15 @@ def run():
 	CGIHandler().run(runSentry)
 
 def runSentry(environ, start_response):
+	lang = choose_lang(Request(environ))
 	try:
 		return (app(environ, start_response))
 	except:
+		html = get_html("500", lang)
 		try:
 			client.captureException()
-			response = Response(get_html("inc/html/500").replace("<!-- log >", "").replace("< /log -->", ""), 500, mimetype="text/html")
+			html = html.replace("<!-- log >", "").replace("< /log -->", "")
 		except:
-			response = Response(get_html("inc/html/500").replace("<!-- nolog>", "").replace("< /nolog -->", ""), 500, mimetype="text/html")
+			html = html.replace("<!-- nolog >", "").replace("< /nolog -->", "")
+		response = Response(html, 500, mimetype="text/html")
 		return response(environ, start_response)
