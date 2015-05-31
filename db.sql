1475961ddfc67ddb148584c0eea21b8a1178b069
Niklas Sombert <niklas@ytvwld.de>
2015-05-31 14:01:24 +0200

    Modified the database to support username- and password-based login or registral.

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ SET time_zone = '+00:00';
 CREATE TABLE `sessions` (
   `sid` varchar(32) NOT NULL,
   `uid` int(11) DEFAULT NULL,
-  `oid` text NOT NULL,
+  `oid` text,
   PRIMARY KEY (`sid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -13,8 +13,9 @@ CREATE TABLE `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` text,
   `openid` text,
+  `pass` varchar(128),
   `email` text,
-  `first_login` tinyint(1) NOT NULL,
+  `first_login` tinyint(1),
   UNIQUE KEY `uid` (`uid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
