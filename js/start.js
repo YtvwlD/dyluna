e54eb7f53e69a3cd473cafe86dd06ac33091b050
Niklas Sombert <niklas@ytvwld.de>
2015-07-22 21:41:12 +0200

    loadscript doesn't seem to be used anymore.

--- js/start.js
+++ js/start.js
@@ -1,12 +1,4 @@
 "use strict";
-//scripts
-function loadscript(script)
-{
-	var js = document.createElement("script");
-	js.src = "js/" + script + ".js";
-	js.type = "text/javascript";
-	document.head.appendChild(js);
-}
 
 //pages
 function loadpage(ppage)
